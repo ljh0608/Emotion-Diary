a052e03077be9c1d5373720b09c0c594552edbb4
ljh0608 <m98m@naver.com>
2022-12-05 11:05:35 +0900

    DiaryItem

---
+++ SimpleDiary/src/DiaryItem.js
@@ -0,0 +1,15 @@
+const DairyItem = ({ author, content, created_date, emotion, id }) => {
+  return (
+    <div className="DiaryItem">
+      <div className="info">
+        <span>
+          작성자 : {author} | 감정정수 : {emotion}{" "}
+        </span>
+        <span className="date">{new Date(created_date).toLocaleString()}</span>
+      </div>
+      <div className="content">{content}</div>
+    </div>
+  );
+};
+
+export default DairyItem;
