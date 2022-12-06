f63bf8f75ea983e76d8d7b38acc41047eca3a92d
ljh0608 <m98m@naver.com>
2022-12-06 19:17:02 +0900

    useMemo사용법

--- SimpleDiary/src/App.js
+++ SimpleDiary/src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import { useState, useRef, useEffect } from "react";
-
-//https://jsonplaceholder.typicode.com/comments
+import { useState, useRef, useEffect, useMemo } from "react";
 
 function App() {
   const [data, setData] = useState([]);
@@ -45,7 +43,7 @@ function App() {
   };
 
   const onRemove = (targetId) => {
-    console.log(`${targetId}가 삭제되었습니다`);
+    // console.log(`${targetId}가 삭제되었습니다`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     console.log(newDiaryList);
     setData(newDiaryList);
@@ -59,10 +57,26 @@ function App() {
     );
   };
 
+  const getDiaryAnalysis = useMemo(() => {
+    //값으로 사용해야함
+
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]); //data.length가 변화하지 않는 이상 계산 없이 같은 리턴을 반환한다
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
       {/* <Lifecycle></Lifecycle> */}
       <DiaryEditor onCreate={onCreate} />
+      <div>전체일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}</div>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
   );
