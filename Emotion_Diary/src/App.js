ad666dcbbc2171f2411934f2171dc344a78bcf29
ljh0608 <m98m@naver.com>
2022-12-15 22:35:49 +0900

    state관리를 위한 reducer와 dispatch 제작

--- Emotion_Diary/src/App.js
+++ Emotion_Diary/src/App.js
@@ -5,74 +5,93 @@ import Home from "./pages/Home";
 import Edit from "./pages/Edit";
 import New from "./pages/New";
 
+import React, { useReducer } from "react";
+import { useRef } from "react";
 //COMPONENTS
 import MyButton from "./components/MyButton";
 import MyHeader from "./components/MyHeader";
 
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
+    default:
+      return state;
+  }
+  return newState;
+};
+
+export const DiaryStateContext = React.createContext();
+export const DiartyDispatchContext = React.createContext();
+
 function App() {
-  // //만약 process.env.PUBLIC_URL이 작동 안하면?
-  // const env = process.env;
-  // env.PUBLIC_URL = env.PUBLIC_URL || "";
+  const [data, dispatch] = useReducer(reducer, []);
+
+  const dataId = useRef(0);
+
+  //CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        data: new Date(date).getTime(),
+        content: content,
+        emotion: emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+
+  //REMOVE
+  const onRemove = (targetId) => {
+    dispatch({ type: "REMOVE", targetId });
+  };
+
+  //EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <MyHeader
-          headText={"App"}
-          leftChild={
-            <MyButton
-              text={"왼쪽 버튼"}
-              onClick={() => {
-                alert("왼쪽 클릭");
-              }}
-            ></MyButton>
-          }
-          rightChild={
-            <MyButton
-              text={"오른쪽 버튼"}
-              onClick={() => {
-                alert("오른쪽 클릭");
-              }}
-            ></MyButton>
-          }
-        ></MyHeader>
-        <h2>App.js</h2>
-        {/*
-        <img
-          src={process.env.PUBLIC_URL + `/assets/emotion5.png`}
-          alt="5오류"
-        /> */}
-        {/* process.env.PUBLIC_URL : 우리가 어떤 위치에 있던 public 디렉토리 경로 */}
-
-        <MyButton
-          text={"버튼"}
-          onClick={() => {
-            alert("버튼 클릭");
-          }}
-          type={"positive"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => {
-            alert("버튼 클릭");
-          }}
-          type={"negative"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => {
-            alert("버튼 클릭");
-          }}
-          type={"default"}
-        />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/new" element={<New />}></Route>
-          <Route path="/edit" element={<Edit />}></Route>
-          <Route path="/diary/:id" element={<Diary />}></Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext.Provider value={data}>
+      <DiartyDispatchContext.Provider value={{ onCreate, onEdit, onRemove }}>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />}></Route>
+              <Route path="/edit" element={<Edit />}></Route>
+              <Route path="/diary/:id" element={<Diary />}></Route>
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiartyDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 }
 
